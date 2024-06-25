5f976ec4de937d1fe6fc4b73f742066d30e93d4e
GawarWawar <gawarwawar@gmail.com>
2024-06-25 04:14:11 +0300

    added flags: -h, --help -v, --volume -s, --sound -lv, --log_level -lf, --log_file

--- ping.py
+++ ping.py
@@ -7,52 +7,119 @@ import time
 import vlc
 
 from utils import check_methods
+from utils import tools
 
 
 def main():
     start_timer = time.perf_counter()
     
-    SITE_TO_PING = "google.com"
+    site_to_ping = "google.com"
+    volume_level = 100
+    sound_to_play = "sounds/Gall_-_Embrace_the_Shadow.oga"
+    is_help_flag = False
+    log_level = logging.DEBUG
+    log_file = 'logs/process.log'
     
-    if len(sys.argv) != 2:
-        print("Usage: python ping.py ping_or_request")
-        return 1
-    elif sys.argv[1] == "ping":
-        server= SITE_TO_PING
-        request_process = check_methods.ping_after_some_time
-        message_starter = "Ping"
+    USAGE_STRING = "Usage: python ping.py ping_or_request [flag] [flag_value]"
+    # TODO: write full description of flags
+    help_string = \
+    """    -h, --help:
+    -v, --volume:
+    -s, --sound:
+    -lv, --log_level:
+    -lf, --log_file:
+    """
+    
+    #TODO: Check the functionality of flags
+    # Additionaly: create tests
+    argc = len(sys.argv)
+    
+    for flag_position, flag in enumerate(sys.argv, 2):
+        next_position = flag_position + 1
         
-    elif sys.argv[1] == "request":
-        server= "https://"+SITE_TO_PING
-        request_process = check_methods.make_request_after_some_time
-        message_starter = "Request"
-
-    else:
-        print("Usage: python ping.py ping_or_request")
-        return 1
+        if tools.check_flag_to_keys(flag, ["-h", "--help"]):
+            is_help_flag = True
+                
+        if tools.check_flag_to_keys(flag, ["-v", "--volume"]):
+            if tools.check_next_flag_bounds(argc, next_position):
+                volume_level = sys.argv[next_position]
+                
+        if tools.check_flag_to_keys(flag, ["-s", "--sound"]):
+            if tools.check_next_flag_bounds(argc, next_position):
+                sound_to_play = sys.argv[next_position]
+                
+        if tools.check_flag_to_keys(flag, ["-lv", "--log_level"]):
+            if tools.check_next_flag_bounds(argc, next_position):
+                log_level = sys.argv[next_position]
+                
+        if tools.check_flag_to_keys(flag, ["-lf", "--log_file"]):
+            if tools.check_next_flag_bounds(argc, next_position):
+                log_file = sys.argv[next_position]
     
-    logger = logging.getLogger(str(datetime.datetime.now()))
-    logger.setLevel(logging.DEBUG)
-    
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger(str(datetime.datetime.now()))                              
+    logger.setLevel(log_level)
     
     # FileHandler
     try:
-        file_handler = logging.FileHandler(Path(__file__).parent.joinpath('logs/process.log'))
+        file_handler = logging.FileHandler(Path(__file__).parent.joinpath(log_file))
     except FileNotFoundError:
         os.mkdir("logs")
-        file_handler = logging.FileHandler(Path(__file__).parent.joinpath('logs/process.log'))
+        file_handler = logging.FileHandler(Path(__file__).parent.joinpath(log_file))
         
-    file_handler.setFormatter(formatter)
+    file_handler.setFormatter(
+        logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    )
     file_handler.setLevel(logging.DEBUG)  
     
     # ConsoleHandler
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)  
-    console_handler.setFormatter(formatter)
+    console_handler.setFormatter(logging.Formatter('%(message)s'))
     
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
+    
+    if sys.argv[1] == "ping":
+        server= site_to_ping
+        request_process = check_methods.ping_after_some_time
+        message_starter = "Ping"
+        
+    elif sys.argv[1] == "request":
+        server= "https://"+site_to_ping
+        request_process = check_methods.make_request_after_some_time
+        message_starter = "Request"
+    else:
+        logger.warning(USAGE_STRING)
+        if is_help_flag:
+            logger.warning(help_string)
+        return 1
+            
+    if is_help_flag:
+        logger.warning(USAGE_STRING)
+        logger.warning(help_string)
+        
+    
+    if argc >= 3:
+        for flag_position, flag in enumerate(sys.argv, 2):
+            next_position = flag_position + 1
+            
+            if tools.check_flag_to_keys(flag, ["-h", "--help"]):
+                if tools.check_next_flag_bounds(argc, next_position):
+                    print(USAGE_STRING)
+                    # TODO: Import flags description
+                    print()
+                    
+            if tools.check_flag_to_keys(flag, ["-v", "--volume"]):
+                if tools.check_next_flag_bounds(argc, next_position):
+                    volume_level = sys.argv[next_position]
+                    
+            if tools.check_flag_to_keys(flag, ["-s", "--sound"]):
+                if tools.check_next_flag_bounds(argc, next_position):
+                    sound_to_play = sys.argv[next_position]
+                    
+            if tools.check_flag_to_keys(flag, ["-s", "--sound"]):
+                if tools.check_next_flag_bounds(argc, next_position):
+                    sound_to_play = sys.argv[next_position]
 
 
     request_count = check_methods.make_request_until_its_successful(
@@ -69,7 +136,10 @@ def main():
     )
 
     
-    sounds_player : vlc.MediaPlayer = vlc.MediaPlayer(Path(__file__).parent.joinpath("sounds/Gall_-_Embrace_the_Shadow.oga"))
+    sounds_player : vlc.MediaPlayer = vlc.MediaPlayer(
+        Path(__file__).parent.joinpath(sound_to_play)
+    )
+    sounds_player.audio_set_volume(volume_level)
     sounds_player.play()
 
     while sounds_player.is_playing() == 0:
