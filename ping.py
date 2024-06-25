f245f3994e12e345c40e5be3cbd4df8d52af3e86
GawarWawar <gawarwawar@gmail.com>
2024-06-25 03:04:42 +0300

    moved ping methods into separate file

--- ping.py
+++ ping.py
@@ -1,122 +1,12 @@
-from collections.abc import Callable
 import datetime
-import requests
-import time
 import logging
 import os
 from pathlib import Path
-import subprocess
 import sys
-from urllib3 import exceptions
+import time
 import vlc
 
-
-
-def ping_after_some_time(
-    server: str,
-    request_count: int,
-    logger: logging.Logger,
-    start_time_to_wait:float,
-    time_to_wait: int = 1,
-    n_of_pings: int = 1
-):
-    end_time_to_wait = time.perf_counter()    
-    while end_time_to_wait - start_time_to_wait < time_to_wait:
-        end_time_to_wait = time.perf_counter()
-    command = ['ping', "-c", str(n_of_pings), str(server)]
-    with open(os.path.abspath(logger.handlers[0].stream.name), "a") as file:
-        pinging = subprocess.call(
-            command, 
-            stdout=file,
-            stderr=file
-        )
-    
-    end_time_to_wait = time.perf_counter()
-    
-    logger.debug(f"Ping numder {request_count} took {end_time_to_wait-start_time_to_wait} time to get")
-    
-    return pinging
-        
-def make_request_after_some_time(
-    server: str,
-    request_count: int,
-    logger: logging.Logger,
-    start_time_to_wait:float = time.perf_counter(),
-    time_to_wait: int = 1
-) -> tuple:
-
-    end_time_to_wait = time.perf_counter()
-    while end_time_to_wait - start_time_to_wait < time_to_wait:
-        end_time_to_wait = time.perf_counter()
-    try: 
-        server_request = requests.get(
-            server, 
-            timeout=10
-        )
-    # If we recieve exeption that indicates that connection wasn`t aquired,
-    # we return None to indicate it  
-    except (requests.exceptions.ReadTimeout, ConnectionResetError, exceptions.ProtocolError, requests.exceptions.ConnectionError) as occured_exception:
-        end_time_to_wait = time.perf_counter()
-        logger.debug(
-            f"While getting request numder {request_count} recieve {occured_exception}. It took {end_time_to_wait-start_time_to_wait} time to get exception"
-        )
-        return None
-    else:
-        end_time_to_wait = time.perf_counter()
-        logger.debug(f"Request numder {request_count} took {end_time_to_wait-start_time_to_wait} time to get")
-        return server_request
-    
-def make_request_until_its_successful(
-    server: str,
-    request_process: Callable,
-    
-    logger: logging.Logger,
-    message_starter: str,
-    
-    request_count: int,
-    start_time_to_wait:float, # Cannot use = time.perf_counter() ->  
-        # If we use it like this, in loop it will bug out and use only the 1st value
-        
-    time_to_wait:int = 1
-) -> tuple:
-
-    # We need server_request_status_code to start up cycle
-    is_going = 1
-    start_time_to_wait = start_time_to_wait - time_to_wait
-    while (
-        is_going != 0
-    ):  
-        request_count += 1
-        server_request = request_process(
-            server=server,
-            request_count=request_count,
-            logger = logger,
-            start_time_to_wait=start_time_to_wait,
-            time_to_wait=time_to_wait
-        )
-        
-        # After make_request_after_some_time returns requests.Response,
-        # we can read its status_code and if it indicates about successful response, programme proceed
-        
-        if not server_request is None:
-            if isinstance(server_request, requests.Response):
-                server_request_status_code = server_request.status_code
-                if server_request_status_code == 200:
-                    is_going = 0
-
-            elif isinstance(server_request, int): 
-                server_request_status_code = server_request
-                is_going = server_request
-               
-            if request_count == 1:
-                logger.info(f"Process started. {message_starter} numder {request_count} returned {server_request_status_code} status_code.")
-            else:
-                logger.debug(f"{message_starter} numder {request_count} returned {server_request_status_code} status_code.")
-        
-        start_time_to_wait = time.perf_counter()
-
-    del start_time_to_wait
-    return request_count
+from utils import check_methods
 
 
 def main():
@@ -127,12 +17,12 @@ def main():
         return 1
     elif sys.argv[1] == "ping":
         server= "google.com"
-        request_process = ping_after_some_time
+        request_process = check_methods.ping_after_some_time
         message_starter = "Ping"
         
     elif sys.argv[1] == "request":
         server= "https://google.com"
-        request_process = make_request_after_some_time
+        request_process = check_methods.make_request_after_some_time
         message_starter = "Request"
 
     else:
@@ -163,7 +53,7 @@ def main():
     logger.addHandler(console_handler)
 
 
-    request_count = make_request_until_its_successful(
+    request_count = check_methods.make_request_until_its_successful(
         server= server,
         request_process= request_process,
         
